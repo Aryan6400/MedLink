35eda1d9d6e19dafb91da3c1fa0775f3c184fd2e
Aryan Singh <74493183+Aryan6400@users.noreply.github.com>
2024-02-05 01:44:35 +0530

    Update About.jsx

--- frontend/src/components/About/About.jsx
+++ frontend/src/components/About/About.jsx
@@ -71,7 +71,7 @@ function About() {
 
                     <Paper elevation={3} className="about-card">
                         <div className="about-img">
-                            <img src="https://raw.githubusercontent.com/Aryan6400/Logo-Assets/main/IMG_9984-min.JPG" target="_blank" rel="noopener noreferrer" />
+                            <img src="https://github.com/Aryan6400/Logo-Assets/blob/main/utpal.jpg?raw=true" target="_blank" rel="noopener noreferrer" />
                         </div>
                         <div className="about-name">Utpal Raj</div>
                         <div className="about-position">AI and Software Developer</div>
